52d7f669ac4c259c06f7d31c459c9c87eb81366b
郇凯 <huankai7@163.com>
2021-04-14 19:49:18 +0800

    2021.4.14

--- 排序算法/冒泡排序/Go/BubbleSort.go
+++ 排序算法/冒泡排序/Go/BubbleSort.go
@@ -4,18 +4,24 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
-	numsArr := strings.Split(scanner.Text(), " ")
-	fmt.Println(numsArr)
-	//result := bubbleSort([]int{numsArr})
+	numsStrArr := strings.Split(scanner.Text(), " ")
+	var numsIntArr []int64
+	for _, value := range numsStrArr {
+		v, _ := strconv.ParseInt(value, 10, 64)
+		numsIntArr = append(numsIntArr, v)
+	}
+	numsIntArr = bubbleSort(numsIntArr)
+	fmt.Println(numsIntArr)
 }
 
-func bubbleSort(arr []int) []int {
+func bubbleSort(arr []int64) []int64 {
 	if arr == nil || len(arr) < 2 {
 		return arr
 	}
